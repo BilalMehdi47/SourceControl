d0eaf2c11a85de9ba472436958f46c5c029513e3
Bilal Mehdi <bilalmehditk@gmail.com>
2024-09-11 15:44:43 +0500

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Bilal Mehdi on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
