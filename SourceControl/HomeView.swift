c24acae02595ad8f5dbdebbe62e6401d3338ad0e
Bilal Mehdi <bilalmehditk@gmail.com>
2024-09-12 11:51:11 +0500

    Add Home View

---
+++ SourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SourceControl
+//
+//  Created by Bilal Mehdi on 12/09/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("New Screen")
+    }
+}
+
+#Preview {
+    HomeView()
+}
