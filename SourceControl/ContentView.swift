d2d2f16bba78619faf8218d120608458f333d346
Bilal Mehdi <bilalmehditk@gmail.com>
2024-09-13 14:30:48 +0500

    [Feature] added and fixed code of content view with scroll view

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -11,39 +11,37 @@ struct ContentView: View {
     
     @State private var title: String = "Press the bell icon and subscribe"
     var body: some View {
-//        VStack {
-//            Image(systemName: "bolt.fill")
-//                .font(.largeTitle)
-//                .imageScale(.large)
-//                .foregroundStyle(.blue)
-//            Text("Hello!")
-//            Text(title)
-//                .font(.caption)
-//            
-//            HStack {
-//                Button("Subscribe")
-//                {
-//                    print("Subscription added")
-//                }
-//                Button(action: {
-//                    
-//                    // Add your button action here
-//                }) {
-//                    Image(systemName: "bell.fill")
-//                        .resizable() // Makes the image resizable
-//                        .frame(width: 13, height: 13) // You can adjust the size of the image
-//                        .foregroundColor(.blue) // Optional: Set the image color
-//                }
-//            }
-//        }
         ScrollView() {
             VStack{
                 ForEach(0..<20) { _ in
-                    Text("Help!!")
+                    Image(systemName: "globe")
+                        .font(.largeTitle)
+                        .imageScale(.large)
+                        .foregroundStyle(.blue)
+                    Text("Hello!")
+                    Text(title)
+                        .font(.caption)
+                    
+                    HStack {
+                        Button("Subscribe")
+                        {
+                            print("Subscription added")
+                        }
+                        Button(action: {
+                            
+                            // Add your button action here
+                        }) {
+                            Image(systemName: "bell.fill")
+                                .resizable() // Makes the image resizable
+                                .frame(width: 13, height: 13) // You can adjust the size of the image
+                                .foregroundColor(.blue) // Optional: Set the image color
+                        }
+                        
+                    }
                 }
             }
+            .padding()
         }
-        .padding()
     }
 }
 
