b3d9d4a7f3b2e8d695819f12d67786ecdd2cbed0
Bilal Mehdi <bilalmehditk@gmail.com>
2024-09-14 11:43:20 +0500

    [Clean] written clean documentation of the things learned

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -5,6 +5,41 @@
 //  Created by Bilal Mehdi on 11/09/2024.
 //
 
+
+/*
+Clone = Copying the repo locally
+Commit = Save ("checkpoint") changes on our current branch
+Stage = Prepare changes for a commit
+Stash = Save changes for later
+Push = Send local commits to remote repo
+Pull = Fetch remote commits to local repo
+Merge = Joining two different branch
+Rebase = Moving one branch on top of another branch
+Cherry picking = Duplicating (copying) one commit from one branch to another
+*/
+
+/*
+
+COMMIT MESSAGES
+
+NEW FEATURE:
+[Feature] Description of the feature
+
+BUG NOT IN PRODUCTION:
+[Bug] Description of the bug
+
+RELEASE:
+[Release] Description of release
+
+BUG IN PRODUCTION:
+[Patch] Description of patch
+
+MUNDANE TASKS:
+[Clean] Description of changes
+
+ */
+
+
 import SwiftUI
 
 struct ContentView: View {
